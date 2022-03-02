dbfc77d40e74b98c0d57e7a0d1053204a3f011ce
luciana-morais <lucianamorais01@yahoo.com.br>
2022-03-02 15:39:07 -0300

    Add GenreResource

---
+++ backend/src/main/java/com/devsuperior/movieflix/resources/GenreResource.java
@@ -0,0 +1,25 @@
+package com.devsuperior.movieflix.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.movieflix.entities.Genre;
+
+
+@RestController
+@RequestMapping(value = "/genres")
+public class GenreResource {
+	
+	@GetMapping
+	public ResponseEntity<List<Genre>> findAll() {
+		List<Genre> list = new ArrayList<>();
+		list.add(new Genre(1L, "Terror"));
+		list.add(new Genre(1L, "Romance"));
+		return ResponseEntity.ok().body(list);
+	}
+}
