118e643a48f166bf0741ce27bdff7f082edd80db
luciana-morais <git@github.com:luciana-morais/>
2022-03-16 11:01:22 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO tb_genre (name) VALUES ('Ação');
+INSERT INTO tb_genre (name) VALUES ('Animação');
+INSERT INTO tb_genre (name) VALUES ('Aventura');
+INSERT INTO tb_genre (name) VALUES ('Drama');
+INSERT INTO tb_genre (name) VALUES ('Ficção científica');
+INSERT INTO tb_genre (name) VALUES ('Terror');
+
+INSERT INTO tb_movie (title, sub_Title, year, img_Url, synopsis, genre_id) VALUES ('Mulher-Maravilha 1984', 'Uma nova era de maravilhas comeÃ§a.', 2020, 'https://www.themoviedb.org/t/p/w533_and_h300_bestv2/srYya1ZlI97Au4jUYAktDe3avyA.jpg', 'Em 1984, Diana Prince entra em conflito com dois inimigos formidÃ¡veis, Maxwell Lord e a Mulher-Leopardo, enquanto reencontra misteriosamente com seu antigo interesse amoroso Steve Trevor.', 1);
+INSERT INTO tb_movie (title, sub_Title, year, img_Url, synopsis, genre_id) VALUES ('Demon Slayer: O Trem Infinito', 'Com sua lÃ¢mina, dÃª um fim ao pesadelo.', 2020, 'https://www.themoviedb.org/t/p/w533_and_h300_bestv2/d1sVANghKKMZNvqjW0V6y1ejvV9.jpg', 'Tanjiro Kamado, junto com Inosuke Hashibira, um garoto criado por javalis que usa uma cabeÃ§a de javali, e Zenitsu Agatsuma, um garoto assustado que revela seu verdadeiro poder quando dorme, embarca no Trem Infinito em uma nova missÃ£o com o Hashira de Fogo, Kyojuro Rengoku, para derrotar um demÃ´nio que tem atormentado o povo e matado os caÃ§adores de oni que se opÃµem a ele!', 2);
+INSERT INTO tb_movie (title, sub_Title, year, img_Url, synopsis, genre_id) VALUES ('Godzilla vs. Kong', 'Um dos dois cairÃ¡.', 2021, 'https://www.themoviedb.org/t/p/w533_and_h300_bestv2/inJjDhCjfhh3RtrJWBmmDqeuSYC.jpg', 'Em uma Ã©poca em que os monstros andam na Terra, a luta da humanidade por seu futuro coloca Godzilla e Kong em rota de colisÃ£o que verÃ¡ as duas forÃ§as mais poderosas da natureza no planeta se confrontarem em uma batalha espetacular para as idades. Enquanto Monarch embarca em uma missÃ£o perigosa em terreno desconhecido e descobre pistas sobre as origens dos TitÃ£s, uma conspiraÃ§Ã£o humana ameaÃ§a tirar as criaturas, boas e mÃ¡s, da face da terra para sempre.', 5);
